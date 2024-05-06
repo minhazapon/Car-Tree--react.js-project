f2049bc5de30a2805d7c1b08cb8e32b585489851
minhazapon <minhajulabedinapon@gmail.com>
2024-05-06 03:21:33 -0700

    added contact files with dom

--- src/Login/Contact.jsx
+++ src/Login/Contact.jsx
@@ -1,7 +1,28 @@
 
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Contact = () => {
+
+    const handleC = e => {
+      
+		e.preventDefault();
+		 
+	
+		 console.log(e.target.email.value)
+		 console.log(e.target.name.value)
+		 console.log(e.target.message.value)
+	
+		 toast("Contact successfully Done");
+	
+	   }
+
+
+
+
+
     return (
         <div className="  animate__animated animate__jackInTheBox animate__repeat-2 mb-10 mt-10 ml-10 mr-10">
             <div>
@@ -17,13 +38,13 @@ const Contact = () => {
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
 						<path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
 					</svg>
-					<span>England, London City</span>
+					<span>Liza Street, New York</span>
 				</p>
 				<p className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
 						<path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
 					</svg>
-					<span>123456789</span>
+					<span>+2546 251 2658</span>
 				</p>
 				<p className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
@@ -34,20 +55,21 @@ const Contact = () => {
 				</p>
 			</div>
 		</div>
-		<form noValidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
+		<form onSubmit={handleC} className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
 			<label className="block">
 				<span className="mb-1">Full name</span>
-				<input type="text" placeholder="Full name" className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-white p-2" />
+				<input type="text" placeholder="Full name" name='name' className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-white p-2" />
 			</label>
 			<label className="block">
 				<span className="mb-1">Email address</span>
-				<input type="email" placeholder="Email address" className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-white p-2" />
+				<input type="email" placeholder="Email address" name='email' className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-white p-2" />
 			</label>
 			<label className="block">
 				<span className="mb-1">Message</span>
-				<textarea rows="3" placeholder="Message" className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-white p-2"></textarea>
+				<textarea rows="3" placeholder="Message" name='message' className="block w-full rounded-md focus:ring focus:ring-opacity-75 text-black focus:ring-violet-400 bg-white p-2"></textarea>
 			</label>
-			<button type="button" className="self-center  w-[200px] text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-white btn text-gray-900 focus:ring-violet-400 hover:ring-violet-400">Submit</button>
+			<input className="self-center  w-[200px] text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-white btn text-gray-900 focus:ring-violet-400 hover:ring-violet-400" type="submit" value="Submit" />
+			<ToastContainer />
 		</form>
 	</div>
 </section>
